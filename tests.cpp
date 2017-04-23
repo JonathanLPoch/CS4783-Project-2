999dfbb0f36204b7c5711adb85ddbcd6bbbf0a1c
wecsam <david@davidtsai.net78.net>
2017-04-22 23:42:18 -0400

    Free resources earlier
    The test program frees allocated memory when it no longer needs it,
    rather than waiting until the end.

--- tests.cpp
+++ tests.cpp
@@ -42,7 +42,10 @@ int main(){
 	// Encrypt it multiply it with the first plaintext. This will test homomorphic addition.
 	paillier_ciphertext_t* ciphertext2 = paillier_enc(NULL, pub, plaintext2, paillier_get_rand_devurandom);
 	paillier_ciphertext_t* product = new paillier_ciphertext_t();
+	mpz_init(product->c);
 	paillier_mul(pub, product, ciphertext, ciphertext2);
+	free(ciphertext);
+	free(ciphertext2);
 	// Add the plaintexts without any crypto. This is plain old addition.
 	mpz_t sum_plain;
 	mpz_init(sum_plain);
@@ -50,18 +53,16 @@ int main(){
 	mpz_mod(sum_plain, sum_plain, pub->n);
 	cout << ", (m'+m_2) mod n=";
 	mpz_out_str(stdout, 10, sum_plain);
+	free(plaintext);
+	free(plaintext2);
 	// Decrypt the product of the ciphertexts to get the sum of the plaintexts.
 	paillier_plaintext_t* sum = paillier_dec(NULL, pub, prv, product);
 	cout << ", D(E(m')*E(m_2) mod n^2)=";
 	mpz_out_str(stdout, 10, sum->m);
 	cout << endl;
-	// Clean up.
+	// Done.
 	free(pub);
 	free(prv);
-	free(ciphertext);
-	free(plaintext);
-	free(ciphertext2);
-	free(plaintext2);
 	free(sum);
 	delete product;
 	return 0;

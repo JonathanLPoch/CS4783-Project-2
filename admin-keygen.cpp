1f772e8521410f99cecc70ccaddbe77af0a2291d
wecsam <david@davidtsai.net78.net>
2017-04-25 17:43:18 -0400

    Added checking that candidate and voter counts are small enough
    The admin-keygen program will now make sure that if every voter votes
    for every candidate, the result will still be valid. If it won't be,
    then the key generation fails.

--- admin-keygen.cpp
+++ admin-keygen.cpp
@@ -3,13 +3,27 @@
 #include "CryptoCommon.h"
 using namespace std;
 
+bool public_key_big_enough(int numVoters, int numCandidates, paillier_pubkey_t* pub){
+	mpz_t v_to_c;
+	mpz_init(v_to_c);
+	// Find {number of voters + 1}^{number of candidates} - 1, which is the
+	// sum of all ballots when all voters vote for all candidates.
+	mpz_ui_pow_ui(v_to_c, numVoters + 1, numCandidates);
+	mpz_sub_ui(v_to_c, v_to_c, 1);
+	// Check that that value is less than the public key.
+	bool result = mpz_cmp(v_to_c, pub->n) < 0;
+	// Clean up.
+	mpz_clear(v_to_c);
+	return result;
+}
+
 int main(){
 	int numCandidates, numVoters;
 	cout << "This program will set up the election." << endl;
-	cout << "How many candidates will there be? ";
+	cout << "At most, how many candidates will there be? ";
 	cout.flush();
 	cin >> numCandidates;
-	cout << "How many voters will there be? ";
+	cout << "At most, how many voters will there be? ";
 	cout.flush();
 	cin >> numVoters;
 	if(numCandidates < 2 || numVoters < 2){
@@ -21,6 +35,10 @@ int main(){
 	paillier_pubkey_t* pub;
 	paillier_prvkey_t* prv;
 	paillier_keygen(KEY_LENGTH, &pub, &prv, paillier_get_rand_devurandom);
+	if(!public_key_big_enough(numVoters, numCandidates, pub)){
+		cerr << "There are too many voters and candidates for a public key of this length.\n";
+		return 2;
+	}
 	// Save the public key along with the numbers of candidates and voters to a file.
 	ofstream ofspub(KEY_FILE_PUBLIC);
 	if(ofspub){
